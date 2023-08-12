ec7b45830192f2b9247bc92cf2a2af1dce0f1530
King Robert Kehinde <73795109+iamkingrobert@users.noreply.github.com>
2023-08-12 12:58:46 +0100

    Commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: true }));
+app.use(cors({ origin: "*" }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;

46114cb2d9c2aa5e16c59b732e5833300841e952
King Robert Kehinde <73795109+iamkingrobert@users.noreply.github.com>
2023-08-14 15:42:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "https://chat-me-restapi-app-git-master-iamkingrobert.vercel.app",
+app.use(cors({ origin: "http://chat-me-restapi-app-git-master-iamkingrobert.vercel.app",
              methods: ["GET", "POST"], allowedHeaders: ["Content-Type"], credentials: true,
              }));
 

7aa5aabd501c7e7e561e7a21999c508994d5d6fa
King Robert Kehinde <73795109+iamkingrobert@users.noreply.github.com>
2023-08-14 15:33:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "https://chat-me-restapi-app-git-master-iamkingrobert.vercel.app",
-             methods: ["GET", "POST"],
-               allowedHeaders: ["Content-Type"],
-    credentials: true,
+             methods: ["GET", "POST"], allowedHeaders: ["Content-Type"], credentials: true,
              }));
 
 app.get("/authenticate", async (req, res) => {
